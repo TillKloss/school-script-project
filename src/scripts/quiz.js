ca0b8abef61021eb2e17a07ba8bb7c4c011ba8de
TillKloss <tilleliaskloss@gmail.com>
2025-03-27 09:23:06 +0100

    quiz finished design

--- src/scripts/quiz.js
+++ src/scripts/quiz.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
     const params = new URLSearchParams(window.location.search);
-    const topic = params.get('topic'); //topic=x wird geholt
-})
+    const topic = params.get('topic'); //topic wird geholt
 
-$.getJSON("questions.json", function (data) {
-    if (!data[topic]) {
-        $("#questions").html("<h2>Dieses Quiz existiert nicht</h2>")
-        return;
-    }
+    //sucht das passende Quiz in der JSON
+    $.getJSON("storage/questions.json", function (data) {
+        //prüft ob das Quiz existiert
+        if (!data[topic]) {
+            $("#questions").html("<h2>Dieses Quiz existiert nicht</h2>")
+        }
 
-    $("#quiz-title")
-})
+        //Fragestellung, Antworten und Position der korrekten Antwort erhalten
+        const quizData = data[topic];
+        const questionData = quizData["questions"][0];
+        const answerData = [questionData["answers"][0], questionData["answers"][1], questionData["answers"][2]];
+        const correctAnswer = questionData["correct"];
+
+        const formattedTopic = topic.charAt(0).toUpperCase() + topic.slice(1);
+        $("#quiz-title").text(formattedTopic);
+        $("#grid-question-card").text(questionData.question);
+        $("#a1").text(answerData[0]);
+        $("#a2").text(answerData[1]);
+        $("#a3").text(answerData[2]);
+    });
+});
