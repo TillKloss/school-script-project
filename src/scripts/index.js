61ec86a5ad240b46349275c072a08b662ceb06b7
TillKloss <tilleliaskloss@gmail.com>
2025-05-20 12:24:26 +0200

    questions added, shop finished, quiz finished, bug fixes, credits removed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ $(document).ready(listener);
 function listener() {
     initCredits();
     initQuestions();
+    updateCreditsUI(getCredits());
     getSolvedQuestions();
     $(".box-card").click(function () {
         const url = $(this).attr("url-data");
