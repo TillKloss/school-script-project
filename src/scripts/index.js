6d7750e730885bd5aa1541ad0b691d8847a7e0a7
TillKloss <tilleliaskloss@gmail.com>
2025-05-22 13:58:37 +0200

    bugs fixed themes changed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,19 @@
 $(document).ready(listener);
 
-//routet an quiz.html
 function listener() {
     initCredits();
     initQuestions();
+    initTheme();
+
+    const savedTheme = getActiveTheme();
+    if (savedTheme) {
+        activateTheme(savedTheme);
+    }
+
     updateCreditsUI(getCredits());
     getSolvedQuestions();
+
+    //routing
     $(".box-card").click(function () {
         const url = $(this).attr("url-data");
         window.location.href = url;
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,3 +3,32 @@ function updateCreditsUI(value) {
     $("[data-credit-display='true']").text(value);
 }
 
+
+//Funktion um das Theme zu aktivieren und zu speichern
+function activateTheme(themeID) {
+    document.body.classList.remove("dark-theme", "cyber-theme", "autumn-theme");
+    if (themeID) {
+        document.body.classList.add(themeID);
+    }
+
+    // CSS-Datei laden oder aktualisieren
+    let existingLink = document.getElementById("custom-theme-css");
+    if (existingLink) {
+        if (themeID) {
+            existingLink.href = "styles/themes/"+themeID+".css";
+            console.log(existingLink.href);
+            existingLink.disabled = false;
+        } else {
+            existingLink.disabled = true;
+        }
+    } else if (themeID) {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "styles/themes/"+themeID+".css";
+        console.log(link.href);
+        link.id = "custom-theme-css";
+        document.head.appendChild(link);
+    }
+
+    setActiveTheme(themeID);
+}
